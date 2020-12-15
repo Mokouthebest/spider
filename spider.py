2c0f09da362a0bc0210ccf8d8aead2c7c6afdb9c
Mokouthebest <865299483@qq.com>
2020-12-15 17:20:50 +0800

    添加项目文件

---
+++ spider.py
@@ -0,0 +1,11 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2020/11/30 0030 23:45
+# @Author : sad
+# @File : test1.py
+# @Software : PyCharm
+
+import bs4 __import__() #网页解析，获取数据
+import re               #正则表达式 进行文字匹配
+import urllib.request,urllib.error #制定URL，获取网页数据
+import xlwt #进行excel操作
+import sqlite3 #进行SQLite 数据库操作
