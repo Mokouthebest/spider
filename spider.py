61cf2cb6d989e724d7d1fa5c3735ca087d5c7c25
Mokouthebest <865299483@qq.com>
2020-12-15 18:15:21 +0800

    重新上传代码

--- spider.py
+++ spider.py
@@ -1,11 +1,132 @@
-# -*- codeing = utf-8 -*-
-# @Time : 2020/11/30 0030 23:45
-# @Author : sad
+# -*- coding = uft-8 -*-
+# @Time :2020/12/1 0001 0:10
+# @Author: Perfect blue
 # @File : test1.py
 # @Software : PyCharm
+from bs4 import BeautifulSoup
+import re
+import urllib.request,urllib.error
+import xlwt
+import sqlite3
 
-import bs4 __import__() #网页解析，获取数据
-import re               #正则表达式 进行文字匹配
-import urllib.request,urllib.error #制定URL，获取网页数据
-import xlwt #进行excel操作
-import sqlite3 #进行SQLite 数据库操作
+def main():
+    baseurl = "https://movie.douban.com/top250?start="
+
+    datalist = getData(baseurl)
+    savepath = "豆瓣电影Top250.xls"
+    #保存数据
+    saveData(datalist,savepath)
+
+
+findLink = re.compile(r'<a href="(.*?)">')  # 创建正则表达式对象，表示规则（字符串的模式）
+    # 影片图片
+findImgSrc = re.compile('<img.*src="(.*?)">', re.S)
+    # 影片片名
+findTitle = re. compile(r'<span class="title">(.*)</span>')
+    # 影片评分
+findRating = re.compile((r'<span class="rating_num" property="v:average">(.*)</span>'))
+    # 找到评价人数
+findJudge = re.compile(r'<span>(\d*)人评价</span>')
+    # 找到概况
+findInq = re.compile(r'<span class="inq">(.*)</span>')
+    # 找到影片的相关内容
+findBd = re.compile(r'<p class="">(.*?)</p>',re.S)
+
+#1.爬取网页
+def getData(baseurl):
+    datalist = []
+    for i in range(0,10):  #调用获取页面信息的函数 10次
+        url = baseurl + str(i*25)
+        html = askURL(url) #保存获取到的网页源码
+
+
+     #2. 逐一解析数据
+        soup = BeautifulSoup(html,"html.parser")
+        for item in soup.find_all('div',class_="item"):         #查找符合要求的字符串，形成列表
+            #print(item)    #测试：查看电影item全部信息
+            data = []           #保存一部电影的所有消息1
+            item =str(item)
+
+            #影片详情的链接
+            link = re.findall(findLink,item)[0]           #re库用来通过正则表达式查找指定的字符串
+            data.append(link)                               #添加链接
+
+            imgSrc = re.findall(findImgSrc,item)[0]
+            data.append(imgSrc)                             #添加图片
+
+            titles = re.findall(findTitle,item)             #片名可能只有一个中文名，没有外国名
+            if(len(titles) == 2):
+                ctitle = titles[0]
+                data.append(ctitle)                         #添加中文名
+                otitle = titles[1].replace("/","")          #去掉无关的符号
+                data.append(otitle)                         # 添加外国名
+            else:
+                data.append(titles[0])
+                data.append(' ')                            #外国名字留空
+
+            rating = re.findall(findRating,item)[0]
+            data.append(rating)                             #添加评分
+
+            judgeNum = re.findall(findJudge,item)[0]
+            data.append(judgeNum)                           #添加评价人数
+
+            inq = re.findall(findInq, item)
+            if len(inq) != 0:
+                inq = inq[0].replace("。", "")  # 去掉句号
+                data.append(inq)  # 添加概述
+            else:
+                data.append(" ")  # 留空
+
+            bd = re.findall(findBd,item)[0]
+            bd = re.sub('<b(\s+)?/r(\s+)?>'," ",bd)         #去掉</br>
+            bd = re.sub('/'," ",bd)                         #替换/
+            data.append(bd.strip())                        #去掉前后的空格
+
+            datalist.append(data)                       #把处理好的一部电影信息放入datalist
+
+            # print(datalist)
+
+    return datalist
+
+
+
+#得到指定一个URL的网页内容
+def askURL(url):
+    head = {    #模拟浏览器投币信息，向豆瓣服务器发送消息
+       "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64;x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 86.0 .4240 .198 Safari / 537.36"
+    }           #用户代理，表示告诉豆瓣服务器，我们是什么类型的机器、浏览器（本质上是告诉浏览器，我们可以接受什么水平的内容）
+
+    request = urllib.request.Request(url,headers=head)
+    html = ""
+    try:
+        response = urllib.request.urlopen(request)
+        html = response.read().decode("utf-8")
+        # print(html)
+    except urllib.error.URLError as e:
+        if hasattr(e,"code"):
+            print(e.code)
+        if hasattr(e,"reason"):
+            print(e.reason)
+    return html
+
+def saveData(datalist,savepath):
+    print("save....")
+    book = xlwt.Workbook(encoding="utf-8",style_compression=0)      #创建workbook对象
+    sheet = book.add_sheet('豆瓣电影Top250',cell_overwrite_ok=True)        #创建工作表
+    col = ("电影详情链接","图片链接","影片中文名","影片外国名","评分","评价数","概况","相关信息")
+    for i in range(0,8):
+        sheet.write(0,i,col[i]) #列名
+    for i in range(0,250):
+        print("第%d条" %(i+1))
+        data = datalist[i]
+        for j in range(0,8):
+            sheet.write(i+1,j,data[j])      #数据
+    book.save(savepath)     #保存
+
+
+
+
+if __name__ == "__main__":  #当程序执行时
+#调用函数
+    main()
+    print("爬取完毕！")
